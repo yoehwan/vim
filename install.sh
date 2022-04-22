1a601d95f1ef8b7ea232b93ea7f7833b9876c8dc
Han <yoehwan@gmail.com>
2022-04-22 09:25:35 +0800

    Update install.sh
    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
+# Update system
+sudo apt-get -y update && apt-get -y upgrade
+sudo apt -y update && apt -y upgrade
+
 # Install Tmux
-# sudo apt install tmux
+sudo apt install tmux
 # Install NeoVim
-# sudo add-apt-repository ppa:neovim-ppa/stable
-# sudo apt-get update
-# sudo apt-get install neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get -y update && apt-get -y upgrade
+sudo apt-get -y install neovim
 
 # Install Nvm
-# curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-# nvm install --lts
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+nvm install --lts
+
