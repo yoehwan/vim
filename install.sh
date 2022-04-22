b3700226462d4e8c64104d6ed19788afaa071c5e
Han <yoehwan@gmail.com>
2022-04-22 10:27:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,13 @@ sudo apt-get -y install neovim
 
 # Install Nvm
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.bashrc
 nvm install --lts
 
+# Install Plug-vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+VIM_DIR=~/.config/nvim
+mkdir -p "$VIM_DIR"
+cp init.vim "$VIM_DIR"/
